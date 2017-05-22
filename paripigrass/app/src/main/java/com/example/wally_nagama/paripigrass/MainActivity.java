c2f8601b0bd35d3fb2e044a29e54f27b138560f8
永間慎太郎 <wally_nagama@nenkeiyaku.local>
2017-05-23 02:58:36 +0900

    USERIDを連番で登録できるようになりました

--- paripigrass/app/src/main/java/com/example/wally_nagama/paripigrass/MainActivity.java
+++ paripigrass/app/src/main/java/com/example/wally_nagama/paripigrass/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.wally_nagama.paripigrass;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Comment;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
-    Button button, userCreateButton;
+    Button button, roomCreateButton;
     EditText editText, userName, roomNumber;
+    int userNum;
+    Context act = this;
+    ChildEventListener childEventListener;
+    String key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +37,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button)findViewById(R.id.button);
-        userCreateButton = (Button)findViewById(R.id.userCreate);
+        roomCreateButton = (Button)findViewById(R.id.userCreate);
         editText = (EditText)findViewById(R.id.edittext);
         userName = (EditText)findViewById(R.id.userName);
         roomNumber = (EditText)findViewById(R.id.roomNumber);
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                //String value = dataSnapshot.getValue(String.class);
-                //Log.d("a", "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("a", "Failed to read value.", error.toException());
-            }
-        });
+        userNum = 0;
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -53,13 +50,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //user作成する
-        userCreateButton.setOnClickListener(new View.OnClickListener(){
+        //roomを作成する
+        roomCreateButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                User user = new User(userName.getText().toString());
+                final User user = new User(userName.getText().toString());
                 String roomId = roomNumber.getText().toString();
-                myRef.child("room" + roomId).child("user1").setValue(user.userName);
+                Room room = new Room(roomId);
+                myRef = database.getReference("room" + roomId);
+                userNum = 0;
+
+                //その部屋のuser数を調べて自分のuserIDを振る
+                //roomのuser数を見て人数を数える
+                //ユーザーのリストなどを見張る
+                childEventListener = new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                        Log.d("a", "onChildAdded:" + dataSnapshot);
+
+                        // A new comment has been added, add it to the displayed list
+                        userNum+=1;
+                        // ...
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                        Log.d("a", "onChildChanged:" + dataSnapshot.getKey());
+
+                        // A comment has changed, use the key to determine if we are displaying this
+                        // comment and if so displayed the changed comment.
+                        //Comment newComment = dataSnapshot.getValue(Comment.class);
+                        //String commentKey = dataSnapshot.getKey();
+                        // ...
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        Log.d("a", "onChildRemoved:" + dataSnapshot.getKey());
+
+                        // A comment has changed, use the key to determine if we are displaying this
+                        // comment and if so remove it.
+                        String commentKey = dataSnapshot.getKey();
+
+                        // ...
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                        Log.d("a", "onChildMoved:" + dataSnapshot.getKey());
+
+                        // A comment has changed position, use the key to determine if we are
+                        // displaying this comment and if so move it.
+                        Comment movedComment = dataSnapshot.getValue(Comment.class);
+                        String commentKey = dataSnapshot.getKey();
+
+                        // ...
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w("a", "postComments:onCancelled", databaseError.toException());
+                        Toast.makeText(act, "Failed to load comments.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                };
+                myRef.addChildEventListener(childEventListener);
+
+                key = myRef.push().getKey();
+
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // ここに1秒後に実行したい処理
+                        Log.d("Firebase", String.format("usernum:%d", userNum));
+                        myRef.child(key).child("userID").setValue(String.valueOf(userNum));
+                        myRef.child(key).child("userName").setValue(user.userName);
+                    }
+                }, 500);
             }
         });
 
---
+++ paripigrass/app/src/main/java/com/example/wally_nagama/paripigrass/Room.java
@@ -0,0 +1,13 @@
+package com.example.wally_nagama.paripigrass;
+
+/**
+ * Created by wally_nagama on 2017/05/23.
+ */
+
+public class Room {
+    public String roomNumber;
+
+    Room(String roomNumber){
+        this.roomNumber = roomNumber;
+    }
+}
