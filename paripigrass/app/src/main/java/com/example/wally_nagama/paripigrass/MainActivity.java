8c35e3c0a71a49baf1f378d52febc69a90265afc
UdonDa <udooon0727@gmail.com>
2017-05-22 20:21:28 +0900

    first commit

--- paripigrass/app/src/main/java/com/example/wally_nagama/paripigrass/MainActivity.java
+++ paripigrass/app/src/main/java/com/example/wally_nagama/paripigrass/MainActivity.java
@@ -27,11 +27,15 @@ public class MainActivity extends AppCompatActivity {
     private TextView txvRec;
     private static final int REQUEST_CODE = 0;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myRef.setValue("Hello, World!");
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -45,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
                 // Failed to read value
                 Log.w("a", "Failed to read value.", error.toException());}
         });
+
+
+
 
                 // へーへーボタン（同意を表す貴重なボタン）
                 txvAction=(TextView) findViewById(R.id.amin_txvAction);
@@ -93,26 +100,40 @@ public class MainActivity extends AppCompatActivity {
         };
 
     // startActivityForResultで起動したアクティビティが終了した時に呼び出される関数
+    // checkchara.resultRecで参照できる
+    CheckCharacter checkchara = new CheckCharacter();
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // 音声認識結果の時
         if(requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
             // 結果文字列リストを取得
             ArrayList<String> results = data.getStringArrayListExtra(
                     RecognizerIntent.EXTRA_RESULTS);
 
+            /* ---1
             // 取得した文字列を結合
             String resultsString = "";
             for(int i = 0; i < results.size(); i++) {
                 resultsString += results.get(i)+";";
+                */
+
+
+            if(results.size() > 0) {
+                // 認識結果候補で一番有力なものを表示
+                txvRec.setText( results.get(0));
+
             }
 
             //トーストで結果表示
             //Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
-            txvRec.setText(resultsString);
+
+            // TextView
+            //txvRec.setText(resultsString);
 
         }
 
-        super.onActivityResult(requestCode, resultCode, data);
+        //super.onActivityResult(requestCode, resultCode, data);    ---1
     }
 }
